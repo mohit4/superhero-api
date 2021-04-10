872444070e9b14ff5102839b9a6bc6c84953cb7b
mohit4 <mohitkumar2801@gmail.com>
2021-04-10 18:59:14 +0530

    Added swagger documentation

--- app/hero/urls.py
+++ app/hero/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
 
 from rest_framework import routers
+from rest_framework_swagger.views import get_swagger_view
 
 from .views import WeaponViewSet
 from .views import AbilityViewSet
@@ -11,4 +12,8 @@ router = routers.DefaultRouter()
 router.register(r'weapons', WeaponViewSet)
 router.register(r'abilities', AbilityViewSet)
 
-urlpatterns = router.urls
+schema_view = get_swagger_view(title='SuperHero API')
+
+urlpatterns = [
+    url('docs/', schema_view),
+] + router.urls
